8537f97e014af71eb915657923cf53c9a1d0f6f3
Terry Bu <burea1124@gmail.com>
2015-07-26 15:44:31 -0400

    init

---
+++ ReactiveCocoaSwiftPractice/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ReactiveCocoaSwiftPractice
+//
+//  Created by Terry Bu on 7/26/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
