c047b395c565b84225d0ff03be75c210e81725e2
Terry Bu <burea1124@gmail.com>
2015-07-26 17:11:09 -0400

    Got filter to work with filterAs extension from zhuchaowe

--- ReactiveCocoaSwiftPractice/ViewController.swift
+++ ReactiveCocoaSwiftPractice/ViewController.swift
@@ -10,25 +10,35 @@ import UIKit
 import ReactiveCocoa
 
 class ViewController: UIViewController {
-
-    @IBOutlet private weak var textField: UITextField!
+    
+    @IBOutlet private weak var firstTextField: UITextField!
+    @IBOutlet private weak var secondTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let textSignal:RACSignal = textField.rac_textSignal()
+        let textSignal:RACSignal = firstTextField.rac_textSignal()
         textSignal.subscribeNextAs { (text:String) -> () in
             println(text)
         }
         
-
+        //A filter is a ‘gate’, filtering-out events which do not match the given condition
+        let secondTextSignal = secondTextField.rac_textSignal()
+        secondTextSignal
+        .filterAs {
+            (textString:NSString!) -> Bool in
+                return textString.length > 3
+        }.subscribeNextAs {
+            (text:String) -> () in
+            println(text)
+        }
         
     }
     
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,10 +49,34 @@ extension RACSignal {
     
     func subscribeNextAs<T>(nextClosure:(T) -> ()) -> () {
         self.subscribeNext {
-            (next: AnyObject!) -> () in
+        (next: AnyObject!) -> () in
             let nextAsT = next! as! T
             nextClosure(nextAsT)
         }
+    }
+    
+    func filterAs<T>(nextClosure:(T!) -> Bool) -> (RACSignal) {
+            return self.filter {
+                (next: AnyObject!) -> Bool in
+                if(next == nil){
+                    return nextClosure(nil)
+                }else{
+                    let nextAsT = next as! T
+                    return nextClosure(nextAsT)
+                }
+            }
+    }
+    
+    func mapAs<T>(nextClosure:(T!) -> AnyObject!) -> (RACSignal) {
+            return self.map {
+                (next: AnyObject!) -> AnyObject! in
+                if(next == nil){
+                    return nextClosure(nil)
+                }else{
+                    let nextAsT = next as! T
+                    return nextClosure(nextAsT)
+                }
+            }
     }
     
     
